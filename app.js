a82c775384abc57ecf1a0bdf2d5528bf76439624
adekiruz <nrmuhamad@gmail.com>
2021-06-28 19:08:43 +0700

    add endpoint for list feed

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const fetch = require('node-fetch')
+const parser = require('xml2json')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/list', async (req, res) => {
+    try {
+        let xmlResult = await fetch('https://www.flickr.com/services/feeds/photos_public.gne')
+            .then(res => res.text())
+
+        let json = parser.toJson(xmlResult, {
+            object: true,
+        })
+
+        res.send(json)
+    } catch (e) {
+        console.log('=== error', e)
+    }
 })
 
 app.listen(port, () => {
